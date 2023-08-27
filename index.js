edbecb09658a81918d1a1d60a3927065b2f5c5ca
unknown <piotrmlodz@gmail.com>
2023-08-27 13:46:28 +0200

    corrected canvas measuring, created Boundry class with draw function

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 const canvas = document.querySelector(".mainCanv");
 const c = canvas.getContext("2d"); // CanvasRenderingContext2D
 
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
+
+
 console.log(canvas);
 console.log(c);
 
+class Boundry {
+    constructor({position}) {
+        this.position = position;
+        this.width = 40;
+        this.height = 40;
+    }
+
+    // funkcja rysująca boundry
+    draw() {
+        c.fillStyle = "blue"
+        c.fillRect(this.position.x, this.position.y, this.width, this.height);
+    }
+}
+
+const boundry = new Boundry({position : {
+    x: 0,
+    y: 0
+}
+});
+
+boundry.draw();
