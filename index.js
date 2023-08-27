03c906ef0abe98e5228c30edefed1d7e572c3be5
unknown <piotrmlodz@gmail.com>
2023-08-27 14:43:15 +0200

    created Player class which creates player character, rules of character movement with event on w, s, a, d keys

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const canvas = document.querySelector(".mainCanv");
 const c = canvas.getContext("2d"); // CanvasRenderingContext2D
+// wszystko co będzie wykonywane na zmiennej c to methody wbudowane w canvas
 
 // CSS widzi canvas jako img przez co nie styluje go poprawnie
 canvas.height = window.innerHeight;
@@ -54,5 +55,66 @@ row.forEach((symbol, symbolIndex) => {
 
 boundaries.forEach((boundary) => {
     boundary.draw();
+})
+
+
+
+// Pacman with movement
+
+class Player {
+    constructor({position, velocity}) {
+        this.position = position;
+        this.velocity = velocity; // prędkość, do ruchów bohatera
+        this.radius = 15;
+    }
+
+    draw() {
+        c.beginPath() //canvas method
+        c.arc(this.position.x, this.position.y, this.radius, 0 , Math.PI *2); // jednostka radian, 0 to punkt startowy rysowania koła, PI w radianach to połowa koła więc *2 to całe koło
+        c.fillStyle = "yellow";
+        c.fill();
+        c.closePath();
+    }
+
+    update() {
+        this.draw();
+        this.position.x += this.velocity.x;  // nowa wartość pozycji x powiekszona o velocity po naciśnięciu przycisku
+        this.position.y += yhis.velocity.y;
+    }
+}
+
+const player = new Player({
+    position: {
+        x: Boundary.width + Boundary.width / 2,
+        y: Boundary.height + Boundary.height / 2
+    },
+    velocity: {
+        x: 0,
+        y: 0
+    }
+});
+
+player.draw();
+
+// ruch bohaterem
+
+window.addEventListener("keydown", ({key}) => {
+    switch (key) {
+        case "w": 
+        player.velocity.y = -5
+        break;
+
+        case "a": 
+        player.velocity.x = -5
+        break;
+
+        case "s": 
+        player.velocity.y = 5
+        break;
+
+        case "d": 
+        player.velocity.x = 5
+        break;
+    }
 })
 
