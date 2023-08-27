981871fe5647ca8e79b0f19ec8c565007b7c9ed2
unknown <piotrmlodz@gmail.com>
2023-08-27 13:23:24 +0200

    Css set measurements for bady and canvas, deleted index.js canvas measurements

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
-const canvas = document.querySelector("canvas");
+const canvas = document.querySelector(".mainCanv");
 const c = canvas.getContext("2d"); // CanvasRenderingContext2D
-
-canvas.width = innerWidth; //objektu window
-canvas.height = innerHeight; //objektu window
-
-
-
-
 
 console.log(canvas);
 console.log(c);
