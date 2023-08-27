e02e732cbe389360d1d1ae33c9771e67b036d70b
unknown <piotrmlodz@gmail.com>
2023-08-27 12:50:20 +0200

    Project setup, created index.html, index.js, style.css with basic structure

---
+++ index.js
@@ -0,0 +1,13 @@
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d"); // CanvasRenderingContext2D
+
+canvas.width = innerWidth; //objektu window
+canvas.height = innerHeight; //objektu window
+
+
+
+
+
+console.log(canvas);
+console.log(c);
+
